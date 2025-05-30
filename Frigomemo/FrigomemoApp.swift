000cd48e71999b6f1d5cee646bb7d066dddcfe57
1h <imwangyihan@outlook.com>
2025-05-30 14:47:03 +0800

    Initial commit

---
+++ Frigomemo/FrigomemoApp.swift
@@ -0,0 +1,17 @@
+	//
+//  FrigomemoApp.swift
+//  Frigomemo
+//
+//  Created by Yihan Wang on 11/8/23.
+//
+
+import SwiftUI
+
+@main
+struct FrigomemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
